a8acdd30232589430fdf394a270e8a770d3f9668
vers-a <vers.angelin@gmail.com>
2016-02-08 16:31:28 +0100

    Clarify tests, improve Interpretor layout

--- SelPluginProj/src/selpluginproj/handlers/Interpretor.java
+++ SelPluginProj/src/selpluginproj/handlers/Interpretor.java
@@ -3,6 +3,7 @@ package selpluginproj.handlers;
 import org.eclipse.emf.ecore.EObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By.ByLinkText;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.xtext.example.mydsl.myDsl.*;
@@ -17,19 +18,23 @@ public class Interpretor {
 	public Interpretor(){}
 	
 	public void execute(Main main){		
-		System.out.println("Let's execute "+main.getFileName()+" !!! ");	
+		System.out.println("Let's execute "+main.getFileName()+" !!! ");
+		
+		//which browser should we use ?
 		switch (main.getBrowserName()) {
 		case "Firefox":
 			SEL_DRIVER = new FirefoxDriver();
 			break;
-
 		default:
 			SEL_DRIVER = new FirefoxDriver();
 			break;
 		}
+		
+		//execute all orders
 		for(EObject e : main.getOrders()){this.execute(e);}		
 	}
-
+	
+	// switch case for ALL orders  TODO : implement all orders
 	private void execute(EObject e) {
 		if(e instanceof Navigate){
 			String url = ((Navigate)e).getUrl();
@@ -50,18 +55,36 @@ public class Interpretor {
 			
 	}
 	
-	private static void click(String name) {
-		SEL_DRIVER.findElement(By.name(name)).click();
-		
-	}
-
-	private static void navigateTo(String url){
-		System.out.println("On va vers " + url);		
-		SEL_DRIVER.get(url);
-	}
 	
-	private static void fillWith(String field,String value){
-		SEL_DRIVER.findElement(By.name(field)).sendKeys(value);
+	
+	
+	
+	
+	
+	/////////////////////////////////  ACTIONS  /////////////////////////////////////
+	
+	private static void click(String name) {fineElementFinder(name).click();}
+	
+
+	private static void navigateTo(String url){SEL_DRIVER.get(url);}
+	
+	
+	private static void fillWith(String field,String value){ fineElementFinder(field).sendKeys(value);}
+	
+	
+	
+	
+	/////////////////////////////////////  UTILITIES  /////////////////////////////////////////
+	
+	
+	private static WebElement fineElementFinder(String clue){
+		WebElement ret = null;
+		try{ // Keep adding some if function is not powerful enough !
+			ret = SEL_DRIVER.findElement(By.name(clue));   //find element using tag 'name'
+		}catch(Exception e){
+			ret = SEL_DRIVER.findElement(By.xpath("[text()[contains(.,'"+clue+"')]")); //find element by text 	
+		} 		
+		return ret;
 	}
 	
 
